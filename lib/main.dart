78ea9d7788bf6b100daf19c089f880433a22ab3e
PyGodoy <godoyvitorio99@gmail.com>
2024-10-16 09:24:12 -0300

    criando projeto swim calculator

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart'; // Importar o provider
+import 'package:swimcalculator/swimcalculatorapp.dart';
+void main() {
+  runApp(SwimCalculatorApp());
+}
+
+class SwimCalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue, // Define a cor primária como azul
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue, // AppBar azul
+          titleTextStyle: TextStyle(
+            color: Colors.white, // Título branco no AppBar
+            fontSize: 20,
+          ),
+        ),
+      ),
+      home: SwimCalculatorPage(),
+    );
+  }
+}
